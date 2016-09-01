d926bb8a7349cad77a6f47032a17f5fdf5d4cb3f
CLgithubSZ <1064134436@qq.com>
2016-09-01 17:09:39 +0800

    去掉console落户的ui，改为json

--- ecps-console/src/main/java/com/cl/ecps/system/controller/SysUserController.java
+++ ecps-console/src/main/java/com/cl/ecps/system/controller/SysUserController.java
@@ -1,14 +1,25 @@
 package com.cl.ecps.system.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cl.ecps.common.entity.SysUser;
+import com.cl.ecps.system.service.SysUserService;
 
 @Controller
-@RequestMapping("/SysUserController")
+@RequestMapping("SysUserController")
 public class SysUserController {
 	
-	@RequestMapping("/toItemIndex.do")
-	public String toItemIndex(){
-		return "item/index";
+	@Resource
+	private SysUserService sysUserService;
+	
+	@RequestMapping("toItemIndex")
+	@ResponseBody
+	public Object toItemIndex(){
+		SysUser sysUser = sysUserService.selectByPrimaryKey(23);
+		return sysUser;
 	}
 }
